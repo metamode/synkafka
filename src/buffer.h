3f586c71d4fc65d0d72c6dc69da586fa0aecc47c
Paul Banks <banks@banksdesigns.co.uk>
2015-04-21 16:19:04 +0100

    Fixing shared pointer and RVO misuse in concern rSYNKAFKAd6ed52d3c

--- src/buffer.h
+++ src/buffer.h
@@ -11,14 +11,14 @@ typedef std::shared_ptr<buffer_t> shared_buffer_t;
 
 inline shared_buffer_t buffer_from_string(const std::string& str)
 {
-	shared_buffer_t b(new buffer_t(str.begin(), str.end()));
-	return std::move(b);
+	shared_buffer_t b = std::make_shared<buffer_t>(str.begin(), str.end());
+	return b;
 }
 
 inline shared_buffer_t buffer_from_string(const char* c_str, size_t len)
 {
 	std::string str(c_str, len);
-	return std::move(buffer_from_string(str));
+	return buffer_from_string(str);
 }
 
 }
